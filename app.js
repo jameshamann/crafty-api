c2843cebbb0131e8279c1532539a4fa88244c945
James Hamann <jameshamann@me.com>
2018-01-22 10:49:18 +0000

    enables cors on node server

--- app.js
+++ app.js
@@ -46,6 +46,12 @@ if (cluster.isMaster) {
 
     app.use(cors())
 
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+
     app.set('view engine', 'ejs');
     app.set('views', __dirname + '/views');
     app.use(bodyParser.urlencoded({extended:false}));
